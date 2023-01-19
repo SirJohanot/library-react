daceef05b7e91ed156a4a26cf57269c815afccfb
SirJohanot <pavelkrafft@gmail.com>
2023-01-19 15:25:14 +0300

    added the Orders page

--- src/components/view/OrderParameters.js
+++ src/components/view/OrderParameters.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedDate, FormattedMessage } from 'react-intl';
 
 export default function OrderParameters({ order }) {
     return (
         <>
-            <p><FormattedMessage id="startDate" />: {order?.startDate}</p>
-            <p><FormattedMessage id="endDate" />: {order?.endDate}</p>
+            <h1>{`${order?.book?.title} | ${order?.user?.login}`}</h1>
+            <p><FormattedMessage id="startDate" />: <FormattedDate value={order?.startDate} /></p>
+            <p><FormattedMessage id="endDate" />: <FormattedDate value={order?.endDate} /></p>
             {order?.returnDate &&
-                <p><FormattedMessage id="returnDate" />: {order?.returnDate}</p>
+                <p><FormattedMessage id="returnDate" />: <FormattedDate value={order?.returnDate} /></p>
             }
-            <p><FormattedMessage id="rentalType" />: {order?.rentalType}</p>
-            <p><FormattedMessage id="rentalState" />: {order?.state}</p>
+            <p><FormattedMessage id="rentalType" />: {order?.rentalType && <FormattedMessage id={order?.rentalType} />}</p>
+            <p><FormattedMessage id="rentalState" />: {order?.state && <FormattedMessage id={order?.state} />}</p>
         </>
     )
 }
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from '../api/axios';
+import PaginationBar from '../components/ui/PaginationBar';
+import OrderParameters from '../components/view/OrderParameters';
+import useAuthentication from '../hooks/useAuthentication';
+
+const GET_ORDERS_METHOD = 'get';
+const GET_ORDERS_URL = '/orders';
 
 export default function Orders() {
+    const { authentication } = useAuthentication();
+
+    const [orders, setOrders] = useState([]);
+    const [displayedOrders, setDisplayedOrders] = useState([]);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            const response = await axios.request({
+                method: GET_ORDERS_METHOD,
+                url: GET_ORDERS_URL,
+                auth: {
+                    username: authentication?.login,
+                    password: authentication?.password
+                }
+            });
+            setOrders(response?.data);
+        }
+        fetchOrders();
+    }, [authentication])
+
     return (
         <section id="main-content">
             <div id="main-content-centered-element">
-                Cool orders
+                {displayedOrders.map((order) =>
+                    <Link to={`/order/${order.id}`} key={order.id}>
+                        <button className="round-bordered-subject block-container">
+                            <OrderParameters order={order} />
+                        </button>
+                    </Link>
+                )}
+                <PaginationBar items={orders} setDisplayedItems={setDisplayedOrders} maxItemsPerPage={5} initialPage={1} />
             </div>
         </section>
     )
