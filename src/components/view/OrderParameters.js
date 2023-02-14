945086d327bf6b4bf9945607eca814ff4c2e3408
SirJohanot <pavelkrafft@gmail.com>
2023-02-14 11:11:03 +0300

    fixed date formatting in OrderParameters

--- src/components/view/OrderParameters.js
+++ src/components/view/OrderParameters.js
@@ -1,14 +1,17 @@
+import moment from 'moment/moment';
 import React from 'react';
-import { FormattedDate, FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 export default function OrderParameters({ order }) {
+    const intl = useIntl();
+
     return (
         <>
             <h1>{`${order?.book?.title} | ${order?.user?.login}`}</h1>
-            <p><FormattedMessage id="startDate" />: <FormattedDate value={order?.startDate} /></p>
-            <p><FormattedMessage id="endDate" />: <FormattedDate value={order?.endDate} /></p>
+            <p><FormattedMessage id="startDate" />: {moment(order?.startDate).format(intl.formatMessage({ id: 'dateFormat' }))} </p>
+            <p><FormattedMessage id="endDate" />: {moment(order?.endDate).format(intl.formatMessage({ id: 'dateFormat' }))}</p>
             {order?.returnDate &&
-                <p><FormattedMessage id="returnDate" />: <FormattedDate value={order?.returnDate} /></p>
+                <p><FormattedMessage id="returnDate" />: {moment(order?.returnDate).format(intl.formatMessage({ id: 'dateFormat' }))}</p>
             }
             <p><FormattedMessage id="rentalType" />: {order?.rentalType && <FormattedMessage id={order?.rentalType} />}</p>
             <p><FormattedMessage id="rentalState" />: {order?.state && <FormattedMessage id={order?.state} />}</p>
