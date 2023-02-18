50b9716905c12107d5bc85abfdd98f5abd5b6219
SirJohanot <pavelkrafft@gmail.com>
2023-02-18 11:14:34 +0300

    added new validations

--- src/utility/validator.js
+++ src/utility/validator.js
@@ -1,5 +1,15 @@
 const humanNameRegex = /^\p{L}+.*$/u;
+const wordRegex = /^[\p{L}\w]+.*$/u;
+const humanNamesRegex = /^\p{L}+.*( *, +\p{L}+.*)*$/u;
 
 export function isHumanName(string) {
     return humanNameRegex.test(string);
+}
+
+export function isAWord(string) {
+    return wordRegex.test(string);
+}
+
+export function isHumanNames(string) {
+    return humanNamesRegex.test(string);
 }
