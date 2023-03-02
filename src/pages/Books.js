74108f78381e92ce676bfb3b93a554ce7c7e730f
SirJohanot <pavelkrafft@gmail.com>
2023-03-02 17:01:45 +0300

    restyled the Books page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import axios from '../api/axios';
 import LoadingBars from '../components/ui/LoadingBars';
 import PaginationBar from '../components/ui/PaginationBar';
-import BookParameters from '../components/view/BookParameters';
 
 const GET_BOOKS_METHOD = 'get';
 const GET_BOOKS_URL = 'books';
@@ -27,15 +27,54 @@ export default function Books() {
         <>
             {books ?
                 <>
-                    {displayedBooks.map((book) =>
-                        !book.deleted &&
-                        <Link to={`/book/${book.id}`} key={book.id}>
-                            <button className="round-bordered-subject block-container">
-                                <BookParameters book={book} />
-                            </button>
-                        </Link>
-                    )}
-                    <PaginationBar items={books} setDisplayedItems={setDisplayedBooks} maxItemsPerPage={5} initialPage={1} />
+                    <div>
+                        <div className="list-header">
+                            <div className="cell">
+                                <span><FormattedMessage id="bookTitle" /></span>
+                            </div>
+                            <div className="cell">
+                                <span><FormattedMessage id="authors" /></span>
+                            </div>
+                            <div className="cell">
+                                <span><FormattedMessage id="genre" /></span>
+                            </div>
+                            <div className="cell">
+                                <span><FormattedMessage id="publisher" /></span>
+                            </div>
+                            <div className="cell">
+                                <span><FormattedMessage id="publishmentYear" /></span>
+                            </div>
+                            <div className="cell">
+                                <span><FormattedMessage id="inStock" /></span>
+                            </div>
+                        </div>
+                        <div className="items-list">
+                            {displayedBooks.map((book) =>
+                                !book.deleted &&
+                                <div className="row" key={book.id}>
+                                    <Link to={`/book/${book.id}`} className="cell link">
+                                        <span>{book?.title}</span>
+                                    </Link>
+                                    <div className="cell">
+                                        <span>{book?.authors?.map(author => author.name).join(", ")}</span>
+                                    </div>
+                                    <div className="cell">
+                                        <span>{book?.genre?.name}</span>
+                                    </div>
+                                    <div className="cell">
+                                        <span>{book?.publisher?.name}</span>
+                                    </div>
+                                    <div className="cell">
+                                        <span>{book?.publishmentYear}</span>
+                                    </div>
+                                    <div className="cell">
+                                        <span>{book?.amount}</span>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <PaginationBar items={books} setDisplayedItems={setDisplayedBooks} maxItemsPerPage={10} initialPage={1} />
                 </>
                 : <LoadingBars />
             }
