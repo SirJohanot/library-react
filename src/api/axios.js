55837c2f97c4a122f26408fed7e38bb77bb0b842
SirJohanot <pavelkrafft@gmail.com>
2023-03-11 11:18:31 +0300

    moved axios base url to an env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_URL ?? 'http://localhost:8080',
     headers: {
         'Content-Type': 'application/json'
     }
