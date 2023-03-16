467b95eb6c4c8ca8aebb1376a73dbb0b87f12e86
SirJohanot <pavelkrafft@gmail.com>
2023-03-16 12:03:05 +0300

    restyled the Unauthorized page

--- src/pages/Unauthorized.js
+++ src/pages/Unauthorized.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { useEffect } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import GoBackButton from '../components/ui/GoBackButton';
 
 export default function Unauthorized() {
+    const intl = useIntl();
+
+    useEffect(() => {
+        document.title = `${intl.formatMessage({ id: 'insufficientPrivileges' })} | ${intl.formatMessage({ id: 'appName' })}`;
+    }, [intl]);
+
+
     return (
         <>
-            <div className="container round-bordered-subject main-page-message">
-                <h1><FormattedMessage id="insufficientPrivileges" /></h1>
-            </div>
-            <div className="buttons-container">
-                <GoBackButton />
+            <div>
+                <div className="container round-bordered-subject main-page-message">
+                    <h1><FormattedMessage id="insufficientPrivileges" /></h1>
+                </div>
+                <div className="buttons-container">
+                    <GoBackButton />
+                </div>
             </div>
         </>
     );
