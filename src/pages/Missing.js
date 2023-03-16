4cd321f645c65c57e7bbadac7c00e3572fbbd289
SirJohanot <pavelkrafft@gmail.com>
2023-03-16 12:03:32 +0300

    restyled the Missing page

--- src/pages/Missing.js
+++ src/pages/Missing.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import React, { useEffect } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import GoBackButton from '../components/ui/GoBackButton';
 
 export default function Missing() {
+    const intl = useIntl();
+
+    useEffect(() => {
+        document.title = intl.formatMessage({ id: 'missingPage' });
+    }, [intl]);
+
     return (
         <>
-            <div className="container round-bordered-subject main-page-message">
-                <h1><FormattedMessage id="missingPage" /></h1>
-            </div>
-            <div className="buttons-container">
-                <GoBackButton />
+            <div>
+                <div className="container round-bordered-subject main-page-message">
+                    <h1><FormattedMessage id="missingPage" /></h1>
+                </div>
+                <div className="buttons-container">
+                    <GoBackButton />
+                </div>
             </div>
         </>
     );
