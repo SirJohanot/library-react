da48addc3be3867bfd64dc589c3461d7a35894ef
SirJohanot <pavelkrafft@gmail.com>
2023-03-17 10:59:59 +0300

    removed unneeded CancelButton

--- src/components/ui/CancelButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-export default function CancelButton() {
-    return (
-        <button className="btn red"><FormattedMessage id="cancel" /></button>
-    );
-}
