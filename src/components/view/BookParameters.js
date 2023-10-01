e30d900c6d64c8194b3ef0a7db6ae570e573f283
Johanot <pavelkrafft@gmail.com>
2023-10-01 09:10:55 +0300

    made the authors parameter name darker in BookParameters

--- src/components/view/BookParameters.js
+++ src/components/view/BookParameters.js
@@ -5,7 +5,7 @@ export default function BookParameters({ book }) {
     return (
         <>
             <h1><span className="parameter-name"><FormattedMessage id="bookTitle" />:</span> {book?.title}</h1>
-            <p><FormattedMessage id="authors" />: {book?.authors?.map(author => author.name).join(", ")}</p>
+            <p><span className="parameter-name"><FormattedMessage id="authors" />:</span> {book?.authors?.map(author => author.name).join(", ")}</p>
             <p><FormattedMessage id="genre" />: {book?.genre?.name}</p>
             <p><FormattedMessage id="publisher" />: {book?.publisher?.name}</p>
             <p><FormattedMessage id="publishmentYear" />: {book?.publishmentYear}</p>
