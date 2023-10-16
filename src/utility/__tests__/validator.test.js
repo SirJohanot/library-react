4c428b4e69a557c7701acdff233f491a9757c35b
Johanot <pavelkrafft@gmail.com>
2023-10-16 09:57:53 +0300

    added a new test for validator.isAHumanName

--- src/utility/__tests__/validator.test.js
+++ src/utility/__tests__/validator.test.js
@@ -4,3 +4,8 @@ it('isHumanName returns true for a human name', () => {
     const result = isHumanName("Lev Orlov");
     expect(result).toBeTruthy();
 });
+
+it('isHumanName returns false for a blank string', () => {
+    const result = isHumanName("");
+    expect(result).toBeFalsy();
+});
