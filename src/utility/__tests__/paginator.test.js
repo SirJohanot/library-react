016f148c03601e89d7460a24773e98acfb154c3a
Johanot <pavelkrafft@gmail.com>
2023-10-25 09:50:09 +0300

    added a new test for getNumberOfPagesToContainEntities

--- src/utility/__tests__/paginator.test.js
+++ src/utility/__tests__/paginator.test.js
@@ -6,3 +6,10 @@ it('getNumberOfPagesToContainEntities returns one when there is not enough entit
     const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
     expect(result).toBe(1);
 });
+
+it('getNumberOfPagesToContainEntities returns three when there is enough entities for three full pages', () => {
+    const entities = ['entity1', 'entity2', 'entity3', 'entity4', 'entity5', 'entity6', 'entity7'];
+    const entitiesPerPage = 3;
+    const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
+    expect(result).toBe(3);
+});
