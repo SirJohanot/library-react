6b99e4a47bbdcdd771ae9eb65c136ffc960a70e3
Johanot <pavelkrafft@gmail.com>
2023-10-27 10:06:56 +0300

    added a new test for paginator.getNumberOfPagesToContainEntities

--- src/utility/__tests__/paginator.test.js
+++ src/utility/__tests__/paginator.test.js
@@ -1,22 +1,29 @@
 import { getNumberOfPagesToContainEntities } from "../paginator";
 
-it('getNumberOfPagesToContainEntities returns one when there is not enough entities for a full page', () => {
+it('getNumberOfPagesToContainEntities returns one when there are not enough entities for a full page', () => {
     const entities = ['entity1', 'entity2', 'entity3'];
     const entitiesPerPage = 5;
     const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
     expect(result).toBe(1);
 });
 
-it('getNumberOfPagesToContainEntities returns three when there is enough entities for three full pages', () => {
+it('getNumberOfPagesToContainEntities returns three when there are enough entities for three full pages', () => {
     const entities = ['entity1', 'entity2', 'entity3', 'entity4', 'entity5', 'entity6', 'entity7'];
     const entitiesPerPage = 3;
     const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
     expect(result).toBe(3);
 });
 
-it('getNumberOfPagesToContainEntities returns two when there is exactly enough entities for two full pages', () => {
+it('getNumberOfPagesToContainEntities returns two when there are exactly enough entities for two full pages', () => {
     const entities = ['entity1', 'entity2', 'entity3', 'entity4'];
     const entitiesPerPage = 2;
     const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
     expect(result).toBe(2);
+});
+
+it('getNumberOfPagesToContainEntities returns zero when there are no entities', () => {
+    const entities = [];
+    const entitiesPerPage = 10;
+    const result = getNumberOfPagesToContainEntities(entities, entitiesPerPage);
+    expect(result).toBe(0);
 });
