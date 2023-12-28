c7bba7f82f2247739bf537be0e0d6c003a566d3c
Johanot <pavelkrafft@gmail.com>
2023-12-28 10:57:30 +0300

    added prop validation to RequireAuthetication

--- src/components/authentication/RequireAuthentication.js
+++ src/components/authentication/RequireAuthentication.js
@@ -1,8 +1,14 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useAuthentication from '../../hooks/useAuthentication';
 
 export default function RequireAuthentication({ allowedRoles }) {
+
+    RequireAuthentication.propTypes = {
+        allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+    };
+
     const { authentication } = useAuthentication();
     const location = useLocation();
 
