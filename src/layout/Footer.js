5c17c2b6071d6fa0e36050ccbce59bbf2f893e52
Johanot <pavelkrafft@gmail.com>
2024-01-12 09:24:46 +0300

    added props validation to Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,7 +1,13 @@
+import { PropTypes } from 'prop-types;';
 import React from 'react';
 import { LOCALES } from '../i18n/locales';
 
 export default function Footer({ setCurrentLocale }) {
+
+    Footer.propTypes = {
+        setCurrentLocale: PropTypes.func.isRequired,
+    };
+
     const handleLocaleChange = (e) => {
         setCurrentLocale(e.target.value);
     }
