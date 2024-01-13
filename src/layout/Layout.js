f18d10c2d3397c159958a139b513a05bcc6a4f11
Johanot <pavelkrafft@gmail.com>
2024-01-13 11:09:46 +0300

    added props validation to Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,9 +1,15 @@
+import { PropTypes } from 'prop-types;';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 
 export default function Layout({ setCurrentLocale }) {
+
+    Layout.propTypes = {
+        setCurrentLocale: PropTypes.func.isRequired,
+    };
+
     return (
         <>
             <Header />
