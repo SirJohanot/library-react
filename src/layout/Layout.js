ae0721ab2c6f226b1472dc88d5d4f45fd1b4ff2c
Johanot <pavelkrafft@gmail.com>
2024-01-19 10:19:14 +0300

    fixed a package naming error in Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,4 +1,4 @@
-import { PropTypes } from 'prop-types;';
+import { PropTypes } from 'prop-types';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
