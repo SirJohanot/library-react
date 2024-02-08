3310000f1d2ad74107259a987ccd6f662a576a8c
Johanot <pavelkrafft@gmail.com>
2024-02-08 07:43:19 +0300

    added a test for if the SearchField renders correctly

---
+++ src/components/ui/__tests__/SearchField.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { LOCALES } from '../../../i18n/locales.js';
+import { messages } from '../../../i18n/messages.js';
+import SearchField from '../SearchField.js';
+
+describe('SearchField', () => {
+    it('renders component correctly', () => {
+        render(<IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><SearchField setSearchedItems={(items) => { }} /></IntlProvider>);
+
+        expect(screen.getByRole('textbox')).toBeInTheDocument;
+    });
+});
