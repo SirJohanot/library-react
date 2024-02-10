fc0e7f24ce0f25d2b02fe7e213dd562f8e0798d1
Johanot <pavelkrafft@gmail.com>
2024-02-10 10:06:59 +0300

    added another test for SearchField

--- src/components/ui/__tests__/SearchField.test.js
+++ src/components/ui/__tests__/SearchField.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { LOCALES } from '../../../i18n/locales.js';
@@ -16,5 +16,16 @@ describe('SearchField', () => {
         render(<IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><SearchField setSearchedItems={(items) => { }} /></IntlProvider>);
 
         expect(screen.getByRole('textbox')).toHaveValue('');
+    });
+
+    it('entering a search query updates the searchLine state', () => {
+        render(<IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><SearchField items={[]} setSearchedItems={(items) => { }} /></IntlProvider>);
+
+        const searchInput = screen.getByRole('textbox');
+        const testLine = 'testValue';
+
+        fireEvent.change(searchInput, { target: { value: testLine } });
+
+        expect(searchInput).toHaveValue(testLine);
     });
 });
