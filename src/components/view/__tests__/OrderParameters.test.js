9a808cbc94d33ecf8c85cccb13295bffe92f611c
Johanot <pavelkrafft@gmail.com>
2024-02-26 09:54:48 +0300

    added a 'displays correct order information' tets for OrderParameters

--- src/components/view/__tests__/OrderParameters.test.js
+++ src/components/view/__tests__/OrderParameters.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import moment from 'moment/moment';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { LOCALES } from '../../../i18n/locales.js';
@@ -12,8 +13,8 @@ describe('OrderParameters', () => {
         startDate: '2022-01-01',
         endDate: '2022-01-10',
         returnDate: '2022-01-12',
-        rentalType: 'physical',
-        state: 'returned',
+        rentalType: 'OUT_OF_LIBRARY',
+        state: 'BOOK_RETURNED',
     };
 
     it('renders order parameters correctly', () => {
@@ -32,5 +33,21 @@ describe('OrderParameters', () => {
         expect(returnDateElement).toBeInTheDocument();
         expect(rentalTypeElement).toBeInTheDocument();
         expect(rentalStateElement).toBeInTheDocument();
+    });
+
+    it('displays correct order information', () => {
+        render(<IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><OrderParameters order={order} /></IntlProvider>);
+
+        const startDateValue = screen.getByText("Start date:").nextSibling.nextSibling.textContent;
+        const endDateValue = screen.getByText("End date:").nextSibling.nextSibling.textContent;
+        const returnDateValue = screen.getByText("Return date:").nextSibling.nextSibling.textContent;
+        const rentalTypeValue = screen.getByText("Rental type:").nextSibling.nextSibling.textContent;
+        const rentalStateValue = screen.getByText("State:").nextSibling.nextSibling.textContent;
+
+        expect(startDateValue).toBe(moment('2022-01-01').format('DD/MM/YYYY'));
+        expect(endDateValue).toBe(moment('2022-01-10').format('DD/MM/YYYY'));
+        expect(returnDateValue).toBe(moment('2022-01-12').format('DD/MM/YYYY'));
+        expect(rentalTypeValue).toBe('out of library');
+        expect(rentalStateValue).toBe('book returned');
     });
 });
