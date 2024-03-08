094eaba532dee51077e8f712c9ca7b78279e8ff3
Johanot <pavelkrafft@gmail.com>
2024-03-08 10:08:15 +0300

    added a 'calls setAuthentication and clears request interceptors on sign-out' test for Header

--- src/layout/__tests__/Header.test.js
+++ src/layout/__tests__/Header.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import { MemoryRouter } from 'react-router-dom';
+import axios from '../../api/axios.js';
 import useAuthentication from '../../hooks/useAuthentication';
 import { LOCALES } from '../../i18n/locales';
 import { messages } from '../../i18n/messages.js';
@@ -104,5 +105,32 @@ describe('Header', () => {
         expect(usersLink).toBeInTheDocument();
         expect(profileLoginElement).toBeInTheDocument();
         expect(signOutButton).toBeInTheDocument();
+    });
+
+    it('calls setAuthentication and clears request interceptors on sign-out', () => {
+        const setAuthenticationMock = jest.fn();
+        useAuthentication.mockReturnValue({
+            authentication: {
+                login: 'john_doe',
+                roles: ['READER'],
+            },
+            setAuthentication: setAuthenticationMock,
+        });
+
+        const mockAxiosInterceptorsRequestClear = jest.spyOn(axios.interceptors.request, 'clear');
+
+        render(
+            <IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}>
+                <MemoryRouter>
+                    <Header />
+                </MemoryRouter>
+            </IntlProvider>
+        );
+
+        const signOutButton = screen.getByRole('button');
+        fireEvent.click(signOutButton);
+
+        expect(setAuthenticationMock).toHaveBeenCalledWith({});
+        expect(mockAxiosInterceptorsRequestClear).toHaveBeenCalled();
     });
 });
