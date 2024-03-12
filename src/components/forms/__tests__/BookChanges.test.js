ff300f02a55a5e619539bb4dd3926d4d496732d5
Johanot <pavelkrafft@gmail.com>
2024-03-12 10:24:34 +0300

    added a 'renders the form with input fields' test for BookChanges

--- src/components/forms/__tests__/BookChanges.test.js
+++ src/components/forms/__tests__/BookChanges.test.js
@@ -1,14 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { IntlProvider } from 'react-intl';
-import { LOCALES } from '../../../i18n/locales.js';
-import { messages } from '../../../i18n/messages.js';
 import BookChanges from '../BookChanges.js';
 
-describe('BookChanges', () => {
-    it('renders form correctly', () => {
-        render(<IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><BookChanges book={{}} setDisabled={(a) => { }} /></IntlProvider>);
+jest.mock('react-intl', () => ({
+    FormattedMessage: (props) => {
+        return <>{props.id}</>
+    }
+}));
 
-        expect(screen.getAllByRole('textbox').length).toBe(4);
+describe('BookChanges', () => {
+    const mockBook = {
+        title: 'Book Title',
+        authors: 'John Doe',
+        genre: 'Fiction',
+        publisher: 'Publisher Name',
+        publishmentYear: '2022',
+        amount: '10',
+    };
+
+    it('renders the form with input fields', () => {
+        render(<BookChanges book={mockBook} setBook={() => { }} handleSubmit={() => { }} error="" setDisabled={() => { }} />);
+
+        expect(screen.getByLabelText('bookTitle:')).toBeInTheDocument();
+        expect(screen.getByLabelText('authors (commaSeparated):')).toBeInTheDocument();
+        expect(screen.getByLabelText('genre:')).toBeInTheDocument();
+        expect(screen.getByLabelText('publisher:')).toBeInTheDocument();
+        expect(screen.getByLabelText('publishmentYear:')).toBeInTheDocument();
+        expect(screen.getByLabelText('inStock:')).toBeInTheDocument();
     });
 });
