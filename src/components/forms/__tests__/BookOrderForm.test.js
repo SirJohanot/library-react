a6b9d465e9735185cafa3deca3caf5457c5b5cc7
Johanot <pavelkrafft@gmail.com>
2024-03-18 09:15:59 +0300

    added a 'changes rental type and days when radio buttons are clicked' test for BookOrderForm

--- src/components/forms/__tests__/BookOrderForm.test.js
+++ src/components/forms/__tests__/BookOrderForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import BookOrderForm from '../BookOrderForm.js';
 
@@ -45,6 +45,28 @@ describe('BookOrderForm', () => {
         const radio21 = screen.getByLabelText('21');
         expect(radio21).toBeInTheDocument();
         expect(radio21).not.toBeChecked();
+    });
+
+    it('changes rental type and days when radio buttons are clicked', () => {
+        render(<BookOrderForm bookId={mockBookId} />);
+
+        const toReadingHallRadio = screen.getByLabelText('TO_READING_HALL');
+        fireEvent.click(toReadingHallRadio);
+
+        expect(toReadingHallRadio).toBeChecked();
+        expect(screen.getByLabelText('OUT_OF_LIBRARY')).not.toBeChecked();
+        expect(screen.getByLabelText('7')).toBeDisabled();
+        expect(screen.getByLabelText('14')).toBeDisabled();
+        expect(screen.getByLabelText('21')).toBeDisabled();
+
+        const outOfLibraryRadio = screen.getByLabelText('OUT_OF_LIBRARY');
+        fireEvent.click(outOfLibraryRadio);
+
+        expect(outOfLibraryRadio).toBeChecked();
+        expect(toReadingHallRadio).not.toBeChecked();
+        expect(screen.getByLabelText('7')).not.toBeDisabled();
+        expect(screen.getByLabelText('14')).not.toBeDisabled();
+        expect(screen.getByLabelText('21')).not.toBeDisabled();
     });
 
 });
