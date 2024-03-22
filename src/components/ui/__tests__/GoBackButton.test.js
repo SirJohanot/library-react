9753072ae0d7b7d9248e360482e4878fed886a39
Johanot <pavelkrafft@gmail.com>
2024-03-22 09:52:29 +0300

    added FormattedMessage mocking to GoBackButton tests

--- src/components/ui/__tests__/GoBackButton.test.js
+++ src/components/ui/__tests__/GoBackButton.test.js
@@ -1,15 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { IntlProvider } from 'react-intl';
 import { BrowserRouter } from 'react-router-dom';
-import { LOCALES } from '../../../i18n/locales.js';
-import { messages } from '../../../i18n/messages.js';
 import GoBackButton from '../GoBackButton.js';
+
+jest.mock('react-intl', () => ({
+    FormattedMessage: (props) => {
+        return <>{props.id}</>
+    }
+}));
 
 describe('GoBackButton', () => {
     it('renders button correctly', () => {
-        render(<BrowserRouter><IntlProvider locale={LOCALES.ENGLISH} messages={messages[LOCALES.ENGLISH]}><GoBackButton /></IntlProvider></BrowserRouter>);
+        render(<BrowserRouter><GoBackButton /></BrowserRouter>);
 
-        expect(screen.getByText('Go Back')).toBeInTheDocument();
+        expect(screen.getByText('goBack')).toBeInTheDocument();
     });
 });
