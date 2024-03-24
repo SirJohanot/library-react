82cc98d55ea74efe9c05722524b5f3dd9e0cccb3
Johanot <pavelkrafft@gmail.com>
2024-03-24 09:14:46 +0300

    added a 'navigates back to the previous page on button click' test for GoBackButton

--- src/components/ui/__tests__/GoBackButton.test.js
+++ src/components/ui/__tests__/GoBackButton.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import GoBackButton from '../GoBackButton.js';
 
@@ -15,9 +15,23 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('GoBackButton', () => {
+
+    beforeEach(() => {
+        mockNavigate.mockClear();
+    });
+
     it('renders button correctly', () => {
         render(<GoBackButton />);
 
         expect(screen.getByText('goBack')).toBeInTheDocument();
+    });
+
+    it('navigates back to the previous page on button click', () => {
+        render(<GoBackButton />);
+
+        const goBackButton = screen.getByText('goBack');
+        fireEvent.click(goBackButton);
+
+        expect(mockNavigate).toHaveBeenCalledWith(-1);
     });
 });
