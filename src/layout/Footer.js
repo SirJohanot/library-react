f04dc832dec59e70b671c5e98c8f261b9b710e1c
Johanot <pavelkrafft@gmail.com>
2024-04-11 10:52:34 +0300

    added light mode to the UI; added a switch between the two modes

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,6 @@
 import { PropTypes } from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Switch from 'react-switch';
 import { LOCALES } from '../i18n/locales';
 
 export default function Footer({ setCurrentLocale }) {
@@ -12,6 +13,19 @@ export default function Footer({ setCurrentLocale }) {
         setCurrentLocale(e.target.value);
     }
 
+    const [isDarkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        let body = document.body;
+        if (isDarkMode) {
+            body.classList.remove('light');
+            body.classList.add('dark');
+        } else {
+            body.classList.remove('dark');
+            body.classList.add('light');
+        }
+    }, [isDarkMode]);
+
     return (
         <footer>
             <select onChange={handleLocaleChange}>
@@ -19,6 +33,17 @@ export default function Footer({ setCurrentLocale }) {
                 <option value={LOCALES.RUSSIAN}>Русский</option>
                 <option value={LOCALES.BELARUSIAN}>Беларуская</option>
             </select>
+            <Switch onChange={() => setDarkMode(prev => !prev)}
+                checked={isDarkMode}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={30}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48} />
         </footer>
     );
 }
