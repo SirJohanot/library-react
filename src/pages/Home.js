24aee6a5e78a3bfc58cd3876e2995a6a3260e993
Johanot <pavelkrafft@gmail.com>
2024-04-16 11:19:13 +0300

    added navigational CardButtons to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
+import BookSymbol from '../components/symbols/BookSymbol';
+import OrdersSymbol from '../components/symbols/OrdersSymbol';
+import ProfileSymbol from '../components/symbols/ProfileSymbol';
+import CardButton from '../components/ui/CardButton';
 import useAuthentication from '../hooks/useAuthentication';
 
 export default function Home() {
@@ -12,8 +16,25 @@ export default function Home() {
     }, [intl]);
 
     return (
-        <div className="main-page-message">
-            <h1><FormattedMessage id="greetingsMessage" />, {authentication?.login}</h1>
+        <div className="home-page">
+            <div className="main-page-message">
+                <h1><FormattedMessage id="greetingsMessage" />, {authentication?.login}</h1>
+            </div>
+            <div className="card-buttons-container">
+                <CardButton symbol={<BookSymbol />} text="books" destination="/books/" />
+                {authentication?.roles?.find((role) => role === 'READER') &&
+                    <CardButton symbol={<OrdersSymbol />} text="myOrders" destination="/orders/" />
+                }
+                {authentication?.roles?.find((role) => role === 'LIBRARIAN') &&
+                    <CardButton symbol={<OrdersSymbol />} text="orders" destination="/orders/" />
+                }
+                {authentication?.roles?.find((role) => role === 'ADMIN') &&
+                    <>
+                        <CardButton symbol={<BookSymbol />} text="addABook" destination="/add-book" />
+                        <CardButton symbol={<ProfileSymbol />} text="users" destination="/users/" />
+                    </>
+                }
+            </div>
         </div>
     );
 }
