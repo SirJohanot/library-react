8655ac6de220f8444362db8973ec9de3f4fdf489
Johanot <pavelkrafft@gmail.com>
2024-05-31 09:38:13 +0300

    added ToastContainer to the app root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { AuthenticationProvider } from './context/AuthenticationProvider';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <BrowserRouter>
       <AuthenticationProvider>
         <Routes>
