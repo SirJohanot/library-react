71ad3af0457ec088692c8581a1a148834978da07
Johanot <pavelkrafft@gmail.com>
2024-06-13 09:26:24 +0300

    removed the genre column from ThemeBooks

--- src/pages/ThemeBooks.js
+++ src/pages/ThemeBooks.js
@@ -62,9 +62,6 @@ export default function ThemeBooks() {
                                 <span><FormattedMessage id="authors" /></span>
                             </div>
                             <div className="cell">
-                                <span><FormattedMessage id="genre" /></span>
-                            </div>
-                            <div className="cell">
                                 <span><FormattedMessage id="publisher" /></span>
                             </div>
                             <div className="cell">
@@ -83,9 +80,6 @@ export default function ThemeBooks() {
                                     </Link>
                                     <div className="important cell">
                                         <span>{book?.authors?.map(author => author.name).join(", ")}</span>
-                                    </div>
-                                    <div className="cell">
-                                        <span>{book?.genre?.name}</span>
                                     </div>
                                     <div className="cell">
                                         <span>{book?.publisher?.name}</span>
