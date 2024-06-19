933e621d04f4ca890b3dec26d2cb265e76457402
Johanot <pavelkrafft@gmail.com>
2024-06-19 18:11:25 +0300

    made some fields in BookParameters hidden by default

--- src/components/view/BookParameters.js
+++ src/components/view/BookParameters.js
@@ -1,6 +1,7 @@
 import { PropTypes } from 'prop-types';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import CollapsibleElements from '../ui/CollapsibleElements';
 
 export default function BookParameters({ book }) {
 
@@ -12,19 +13,21 @@ export default function BookParameters({ book }) {
         <>
             <h1><span className="parameter-name"><FormattedMessage id="bookTitle" />:</span> {book?.title}</h1>
             <p><span className="parameter-name"><FormattedMessage id="authors" />:</span> {book?.authors?.map(author => author.name).join(", ")}</p>
-            <p><span className="parameter-name"><FormattedMessage id="editors" />:</span> {book?.editors?.map(editor => `${editor.role} ${editor.name}`).join(", ")}</p>
-            <p><span className="parameter-name"><FormattedMessage id="genre" />:</span> {book?.genre?.name}</p>
             <p><span className="parameter-name"><FormattedMessage id="publisher" />:</span> {book?.publisher?.name}, {book?.publisher?.postalCode}, {book?.publisher?.address}</p>
-            <p><span className="parameter-name"><FormattedMessage id="printingHouse" />:</span> {book?.printingHouse?.name}, {book?.printingHouse?.postalCode}, {book?.printingHouse?.address}</p>
             <p><span className="parameter-name"><FormattedMessage id="publicationYear" />:</span> {book?.publicationYear}</p>
             <p><span className="parameter-name"><FormattedMessage id="publicationLocation" />:</span> {book?.publicationLocation}</p>
-            <p><span className="parameter-name"><FormattedMessage id="description" />:</span> {book?.description}</p>
-            <p><span className="parameter-name"><FormattedMessage id="pagesNumber" />:</span> {book?.pagesNumber}</p>
             <p><span className="parameter-name">ISBN:</span> {book?.isbn}</p>
-            <p><span className="parameter-name"><FormattedMessage id="udc" />:</span> {book?.udc}</p>
-            <p><span className="parameter-name"><FormattedMessage id="bbc" />:</span> {book?.bbc}</p>
-            <p><span className="parameter-name"><FormattedMessage id="authorIndex" />:</span> {book?.authorIndex}</p>
             <p><span className="parameter-name"><FormattedMessage id="inStock" />:</span> {book?.amount}</p>
+            <CollapsibleElements>
+                <p><span className="parameter-name"><FormattedMessage id="editors" />:</span> {book?.editors?.map(editor => `${editor.role} ${editor.name}`).join(", ")}</p>
+                <p><span className="parameter-name"><FormattedMessage id="genre" />:</span> {book?.genre?.name}</p>
+                <p><span className="parameter-name"><FormattedMessage id="printingHouse" />:</span> {book?.printingHouse?.name}, {book?.printingHouse?.postalCode}, {book?.printingHouse?.address}</p>
+                <p><span className="parameter-name"><FormattedMessage id="description" />:</span> {book?.description}</p>
+                <p><span className="parameter-name"><FormattedMessage id="pagesNumber" />:</span> {book?.pagesNumber}</p>
+                <p><span className="parameter-name"><FormattedMessage id="udc" />:</span> {book?.udc}</p>
+                <p><span className="parameter-name"><FormattedMessage id="bbc" />:</span> {book?.bbc}</p>
+                <p><span className="parameter-name"><FormattedMessage id="authorIndex" />:</span> {book?.authorIndex}</p>
+            </CollapsibleElements>
         </>
     );
 }
